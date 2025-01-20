2de49b00e2e91bb2f5b6e0aa96975df06aa63994
bryan <bleonv02@hotmail.com>
2025-01-20 10:58:18 -0500

    Crea función Agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo(){
+    let nombreAgregado = document.getElementById('amigo').value;
+    if(nombreAgregado == ''){
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(nombreAgregado);
+        alert("Amigo agregado");
+        document.getElementById('amigo').value = '';
+        console.log(amigos);
+    }
+    
+}
